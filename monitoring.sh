7cd14d8597264def675510955fc564e6fbbe6981
Hyeonsong Kim <ganadabang@gmail.com>
2021-06-15 14:07:35 +0900

    Fix parsing cpuload

--- monitoring.sh
+++ monitoring.sh
@@ -6,17 +6,17 @@ USAGE=$(printf %.2f $(echo "100*$USED/$TOTAL" | bc -l))
 IP=$(ip addr | grep  -v host | grep "inet " | awk '{print $2}' | cut -d"/" -f 1)
 MAC=$(ip addr | grep  -v host | grep link/ether | awk '{print $2}' | cut -d"/" -f 1)
 LVM=$(lsblk | awk '/root/ {print $6}')
-DU=$(df  --total | grep total | awk '{print $3}')
+DU=$(df --total | grep total | awk '{print $3}')
 DUSED=$(printf %d $(echo $DU/1024 | bc))
 DTOTAL=$(df --total -h | grep total | awk '{print $2}')
 DUSAGE=$(df --total | grep total | awk '{print $5}')
-
+CUSAGE=$(top -bn1 | grep Cpu | awk '{printf("%.1f%%", $2 + $4)}')
 echo "#Architecture: "$(uname --all)
 echo "#CPU physical : "$(nproc)
 echo "#vCPU : "$(cat /proc/cpuinfo | grep processor | wc -l)
 echo "#Memory Usage: "$USED/${TOTAL}MB '('$USAGE%')'
 echo "#Disk Usage: "$DUSED/${DTOTAL}b '('$DUSAGE')'
-echo "#CPU load: "$(uptime | awk '{print $8}' | cut -d , -f 1)
+echo "#CPU load: $CUSAGE"
 echo "#Last boot: "$(who -b | awk '{print $3" "$4}')
 echo "#LVM use: "$(if [ ${LVM} = "lvm" ];
 then
